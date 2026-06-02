f381f21f84c9df0950d7a9eed08faa74d3f9949e
AbelYong <zs23014089@estudiantes.uv.mx>
2026-06-02 14:44:08 -0600

    fix: mysql db failure ending main process

--- src/drizzle/db.ts
+++ src/drizzle/db.ts
@@ -5,16 +5,25 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const connection = await mysql.createConnection({
+const pool = await mysql.createConnection({
     host: process.env["DB_HOST"] as string,
     port: Number(process.env["DB_PORT"]),
     user: process.env["DB_USER"] as string,
     password: process.env["DB_PASSWORD"] as string,
-    database: process.env["DB_DATABASE"] as string
+    database: process.env["DB_DATABASE"] as string,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+    enableKeepAlive: true,
+    keepAliveInitialDelay: 0
+});
+
+pool.on('error', (err: any) => {
+    console.error('Error crítico en el pool de MySQL:', err.message);
 });
 
 export const db = drizzle({ 
-    client: connection,
+    client: pool,
     relations: relations
 });
 
