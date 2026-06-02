421a9047f9c382eefd510b86cc438157d097afbf
AbelYong <zs23014089@estudiantes.uv.mx>
2026-06-02 13:34:39 -0600

    trigger sonarcloud anaylisis

--- src/index.ts
+++ src/index.ts
@@ -70,3 +70,5 @@ app.use(errorHandler);
 app.listen(port, () => {
     console.log(`Mercado libre Node escuchando en el puerto ${port} en entorno ${process.env["NODE_ENV"]}`);
 });
+
+//change to trigger sonarcloud analysis
